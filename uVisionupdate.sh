1a84a6e7c12e6be85822d99a1215271b41f11fb2
Dominic Fandrey <lonkamikaze@users.noreply.github.com>
2012-10-11 12:42:31 +0200

    Remove obsolete IncludePath entries from the uvproj
    changed uVision/hsk_libs.uvproj
    - Remove all the redundant IncludePath entries
    
    changed uVisionupdate.sh
    - Be more specific about the IncludePath to update
    - Use dos style paths, i.e. \ instead of /

--- uVisionupdate.sh
+++ uVisionupdate.sh
@@ -5,12 +5,18 @@ IFS='
 
 eval "$(make printEnv)"
 
-overlays="$(awk -f scripts/overlays.awk $(find src/ -name \*.c))"
+echo "Preparing header include directories ..." 1>&2
+CANDIR="$(echo "$CANDIR" | tr '/' '\\')"
 
+echo "Getting call tree changes for overlay optimisation ..." 1>&2
+overlays="$(awk -f scripts/overlays.awk $(find src/ -name \*.c))"
+echo "$overlays" | sed -e 's/^/	/' -e 's/[[:cntrl:]]$//' 1>&2
+
+echo "Updating uVision/hsk_dev.uvproj ..." 1>&2
 cp uVision/hsk_libs.uvproj uVision/hsk_libs.uvproj.bak
 awk -f scripts/xml.awk uVision/hsk_libs.uvproj.bak \
-        -search:IncludePath \
-        -set:"../$CANDIR" \
+        -search:Target51/C51/VariousControls/IncludePath \
+        -set:"..\\$CANDIR" \
         -select:/ \
 	-search:OverlayString \
 	-set:"$overlays" \
