bd5f2aa1293c9682bc2bdf6af6b4e2195a3f4c73
Dominic Fandrey <kamikaze@bsdforen.de>
2013-12-01 14:26:12 +0100

    Add a simple doxygen filter for unsupported scripting languages
    added scripts/file2doxygen.awk
    - Produces file documentation for unsupported languages
    - The code is discarded
    
    changed conf/doxygen.scripts
    - Use the file2doxygen.awk filter to generate .sh documentation
    
    changed scripts/build.sh
    changed scripts/dbc.sh
    changed scripts/sdcc.sh
    changed scripts/testver.sh
    - Add documentation

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,60 @@
-#!/bin/sh
+#!/bin/sh -f
+#
+# This script produces a make file with build instructions for a given
+# C source directory.
+#
+# @param 1
+#	The source direcotry to generate build instrucitons for
+# @param @
+#	The remaining command line arguments are treated as include
+#	directories
+#
+# This script performs two major steps:
+# - Produce build instructions for each <tt>.c</tt> file
+# - Produce linking instructions for each \c main function
+#
+# The output contains the meta target \c build, which links all programs.
+# The meta target \c all simply builds everything, even targets not required
+# for linking.
+#
+# \section env Environment Variables
+#
+# This script uses or sets certain environment variables:
+#
+# - AWK:
+#	- This variable contains the AWK interpreter to use
+#	- Defaults to \c awk
+# - CPP:
+#	- Used by some secondory scripts to process C code
+#	- Not set by default
+# - LIBPROJDIR:
+#	- This variable is set to the relative path to the parent directory
+#	  of the script and passed on to some AWK scripts
+#	- Left empty if the script was called from the parent directory of
+#	  the script
+#
+# \section calls Secondary Scripts
+#
+# This script calls other scripts during operation:
+# - depends.awk
+# - links.awk
+#
+# The following scripts are called from the generated make file
+# - sanity.awk
+#
+# \section make Make Requirements
+#
+# The generated make file expects the following variables to be set:
+# | Variable | Description
+# |----------|-----------------------------------------------------------------
+# | BUILDDIR | The directory to dump compiler output to
+# | CC       | The C compiler
+# | CFLAGS   | Compiler arguments
+# | HEXSUFX  | The filename suffix for the hex file containing the linked code
+# | OBJSUFX  | The filename suffix for object files
+#
+
+# Force noglob mode
 set -f
 
 IFS='
--- scripts/dbc.sh
+++ scripts/dbc.sh
@@ -1,4 +1,24 @@
-#!/bin/sh
+#!/bin/sh -f
+#
+# This script produces a make file with instructions to generate C headers
+# from Vector DBC files.
+#
+# @param @
+#	The directories containing <tt>.dbc</tt> files
+#
+# The script uses the \c LIBPROJDIR and \c AWK environment variables similar
+# to the way build.sh uses them.
+#
+# For each <tt>.dbc</tt> file a target to create an UTF-8 version of the file
+# is created. The file is expected to use CP1252 (Windows encoding), the
+# conversion is performed by \c iconv.
+#
+# The next target uses dbc2c.awk to generate the desired C headers.
+#
+# The meta target \c dbc generates all headers.
+#
+
+# Force noglob mode
 set -f
 
 IFS='
