55216b8c3c2a1d58a89126cee49a68cc4f9f177c
Dominic Fandrey <kamikaze@bsdforen.de>
2017-01-19 23:07:40 +0100

    Rename µVision project file

--- uVisionupdate.sh
+++ uVisionupdate.sh
@@ -27,16 +27,16 @@ echo "Getting call tree changes for overlay optimisation ..." 1>&2
 overlays="$($AWK -f scripts/overlays.awk $(find src/ -name *.c) -I$INCDIR -I$GENDIR)"
 echo "$overlays" | sed -e 's/^/	/' -e 's/[[:cntrl:]]$//' 1>&2
 
-echo "Updating uVision/hsk_libs.uvproj ..." 1>&2
-cp uVision/hsk_libs.uvproj uVision/hsk_libs.uvproj.bak
-$AWK -f scripts/xml.awk uVision/hsk_libs.uvproj.bak \
+echo "Updating uVision/hsk-libs.uvproj ..." 1>&2
+cp uVision/hsk-libs.uvproj uVision/hsk-libs.uvproj.bak
+$AWK -f scripts/xml.awk uVision/hsk-libs.uvproj.bak \
         -search:Target51/C51/VariousControls/IncludePath \
         -set:"..\\$_GENDIR" \
         -select:/ \
 	-search:OverlayString \
 	-set:"$overlays" \
 	-select:/ \
-	-print > uVision/hsk_libs.uvproj \
-		&& rm uVision/hsk_libs.uvproj.bak \
-		|| mv uVision/hsk_libs.uvproj.bak uVision/hsk_libs.uvproj
+	-print > uVision/hsk-libs.uvproj \
+		&& rm uVision/hsk-libs.uvproj.bak \
+		|| mv uVision/hsk-libs.uvproj.bak uVision/hsk-libs.uvproj
 
